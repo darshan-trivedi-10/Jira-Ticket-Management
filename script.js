49ab03c3152b011f527be941c8e391f41f8cf9d1
datshan-trivedi-10 <dbtrivedi03@gmail.com>
2022-03-21 08:49:57 +0530

    Ticket Generate Complete

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 let addBtn = document.querySelector(".add-btn");
-console.log(addBtn);
+let addFlag = false;
+let modalCont = document.querySelector(".modal-cont");
+let mainCont = document.querySelector(".main-cont");
+let textAreaCont = document.querySelector(".textarea-cont");
 
 addBtn.addEventListener("click", (e) => {
     // Display Modal
     // Generate Ticket
+    addFlag = !addFlag;
+    if (addFlag) {
+        modalCont.style.display = "flex";
+    } else {
+        modalCont.style.display = "none";
+    }
 });
 
+modalCont.addEventListener("keydown", (e) => {
+    let key = e.key;
+    console.log(key);
+    if (key === "Shift") {
+        createTicket();
+        modalCont.style.display = "none";
+        addFlag = false;
+        textAreaCont.value = "";
+    }
+})
+
+function createTicket() {
+    let ticketCont = document.createElement("div");
+    ticketCont.setAttribute("class", "ticket-cont");
+    ticketCont.innerHTML = `
+    <div class="ticket-cont">
+    <div class="ticket-color"></div>
+    <div class="ticket-id">#sample_id</div>
+    <div class="ticket-area">
+        Harum cum blanditiis vitae tempore dolorem amet. Minus </div>
+</div>
+    `;
+
+    mainCont.appendChild(ticketCont);
+}
