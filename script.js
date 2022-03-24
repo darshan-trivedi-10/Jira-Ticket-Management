5990f8593279099f0e6413e881363b93871b860d
datshan-trivedi-10 <dbtrivedi03@gmail.com>
2022-03-24 16:44:34 +0530

    Project

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 let addBtn = document.querySelector(".add-btn");
-let addFlag = false;
+let removeBtn = document.querySelector(".remove-btn");
+let addFlag = true;
 let modalCont = document.querySelector(".modal-cont");
 let mainCont = document.querySelector(".main-cont");
 let textAreaCont = document.querySelector(".textarea-cont");
+let allPriorityColors = document.querySelectorAll(".priority-color");
+let colors = ["lightpink", "lightblue", "lightgreen", "black"];
+let modalPriorityColor = colors[3];
+let removeFlag = false;
+let lockClass = "fa-lock";
+let unlockClass = "fa-lock-open";
+
+if (addFlag) {
+    modalCont.style.display = "none";
+
+}
+
+
+// Listener for modal coloring
+allPriorityColors.forEach(function (colorElem, idx) {
+    colorElem.addEventListener("click", (e) => {
+        allPriorityColors.forEach((colorRemove, idx) => {
+            colorRemove.classList.remove("border");
+        })
+
+        colorElem.classList.add("border");
+        modalPriorityColor = colorElem.classList[1];
+    })
+})
+
 
 addBtn.addEventListener("click", (e) => {
     // Display Modal
@@ -15,28 +41,75 @@ addBtn.addEventListener("click", (e) => {
     }
 });
 
+removeBtn.addEventListener("click", (e) => {
+    removeFlag = !removeFlag;
+})
+
 modalCont.addEventListener("keydown", (e) => {
     let key = e.key;
-    console.log(key);
     if (key === "Shift") {
-        createTicket();
+        createTicket(modalPriorityColor, textAreaCont.value, shortid());
         modalCont.style.display = "none";
         addFlag = false;
         textAreaCont.value = "";
     }
 })
 
-function createTicket() {
+function createTicket(ticketColor, ticketTask, ticketId) {
     let ticketCont = document.createElement("div");
     ticketCont.setAttribute("class", "ticket-cont");
     ticketCont.innerHTML = `
     <div class="ticket-cont">
-    <div class="ticket-color"></div>
-    <div class="ticket-id">#sample_id</div>
-    <div class="ticket-area">
-        Harum cum blanditiis vitae tempore dolorem amet. Minus </div>
-</div>
-    `;
-
+    <div class="ticket-color ${ticketColor}"></div>
+    <div class="ticket-id">#${ticketId}</div>
+    <div class="ticket-area">${ticketTask}</div>
+    </div>
+    <div class="ticket-lock">
+    <i class="fas fa-lock"></i>
+    </div>
+`;
     mainCont.appendChild(ticketCont);
+    handleRemoval(ticketCont);
+    handleLock(ticketCont);
+    handleColor(ticketCont);
+}
+
+
+function handleRemoval(ticket) {
+    if (removeFlag) {
+        ticket.remove();
+    }
+}
+
+function handleLock(ticket) {
+    let ticketLockElem = ticket.querySelector(".ticket-lock");
+    let ticketLock = ticketLockElem.children[0];
+    let ticketTaskArea = ticket.querySelector(".ticket-area")
+    ticketLock.addEventListener("click", (e) => {
+        if (ticketLock.classList.contains(lockClass)) {
+            ticketLock.classList.remove(lockClass);
+            ticketLock.classList.add(unlockClass);
+            ticketTaskArea.setAttribute("contenteditable", "true");
+        } else {
+            ticketLock.classList.remove(unlockClass);
+            ticketLock.classList.add(lockClass);
+            ticketTaskArea.setAttribute("contenteditable", "false");
+        }
+    })
+}
+
+function handleColor(ticket) {
+    let ticketColor = ticket.querySelector(".ticket-color");
+    ticketColor.addEventListener("click", (e) => {
+        let currTicketColor = ticketColor.classList[1];
+        let currentticolorIdx = colors.findIndex((colors) => {
+            return currTicketColor === colors;
+        })
+        console.log(currTicketColor, " ", currentticolorIdx);
+        currentticolorIdx++;
+        currentticolorIdx = currentticolorIdx % colors.length;
+        let newTicketColor = colors[currentticolorIdx];
+        ticketColor.classList.remove((currTicketColor));
+        ticketColor.classList.add(newTicketColor);
+    })
 }
